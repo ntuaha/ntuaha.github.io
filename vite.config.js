555426510861ce4b69da88e132fc5d2c913a3a9d
aha <ntuaha@gmail.com>
2025-04-13 02:59:36 +0800

    update path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ntuaha/', // 與 repo 名稱相同
+  base: '/', // 與 repo 名稱相同
   plugins: [vue()],
 })
